ed8da8b165535251820eb8d405b8156601326a96
Svein-Tore <svenar89@gmail.com>
2026-01-29 10:19:13 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,18 +5,40 @@ function drive (mottatt: string) {
         basic.pause(svingetid)
         servos.P1.run(0)
         servos.P0.run(0)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
     } else if (mottatt == "Pil H") {
         servos.P1.run(0)
         servos.P0.run(max_fart)
         basic.pause(svingetid)
         servos.P1.run(0)
         servos.P0.run(0)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
     } else if (mottatt == "Annet") {
         servos.P1.run(-1 * max_fart)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
         servos.P0.run(max_fart)
     } else if (mottatt == "stopp") {
         servos.P1.stop()
         servos.P0.stop()
+        basic.showString("S")
     }
 }
 radio.onReceivedString(function (receivedString) {
@@ -30,6 +52,3 @@ radio.setGroup(1)
 svingetid = 2000
 mottatt = ""
 max_fart = 20
-basic.forever(function () {
-	
-})
