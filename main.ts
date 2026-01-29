afb1a0a26d776eae1a56da65eff323c721547996
Svein-Tore <svenar89@gmail.com>
2026-01-29 09:54:42 +0100

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,35 @@
+function drive (mottatt: string) {
+    if (mottatt == "Pil V") {
+        servos.P1.run(-1 * max_fart)
+        servos.P0.run(0)
+        basic.pause(svingetid)
+        servos.P1.run(0)
+        servos.P0.run(0)
+    } else if (mottatt == "Pil H") {
+        servos.P1.run(0)
+        servos.P0.run(max_fart)
+        basic.pause(svingetid)
+        servos.P1.run(0)
+        servos.P0.run(0)
+    } else if (mottatt == "Annet") {
+        servos.P1.run(-1 * max_fart)
+        servos.P0.run(max_fart)
+    } else if (mottatt == "stopp") {
+        servos.P1.stop()
+        servos.P0.stop()
+    }
+}
+radio.onReceivedString(function (receivedString) {
+    mottatt = receivedString
+    drive(mottatt)
+})
+let max_fart = 0
+let mottatt = ""
+let svingetid = 0
+radio.setGroup(1)
+svingetid = 2000
+mottatt = ""
+max_fart = 20
+basic.forever(function () {
+	
+})
