19eface5a5e2a75244728f3954e388c2a7b18a30
Svein-Tore <svenar89@gmail.com>
2026-02-02 21:14:26 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    mottatt = "Annet"
+})
 function drive (mottatt: string) {
     if (mottatt == "Pil V") {
-        servos.P1.run(-1 * max_fart)
+        servos.P2.run(-1 * max_fart)
         servos.P0.run(0)
         basic.pause(svingetid)
-        servos.P1.run(0)
-        servos.P0.run(0)
         basic.showLeds(`
             . . # . .
             . # . . .
@@ -12,12 +13,12 @@ function drive (mottatt: string) {
             . # . . .
             . . # . .
             `)
+        servos.P0.stop()
+        servos.P2.stop()
     } else if (mottatt == "Pil H") {
-        servos.P1.run(0)
+        servos.P2.run(0)
         servos.P0.run(max_fart)
         basic.pause(svingetid)
-        servos.P1.run(0)
-        servos.P0.run(0)
         basic.showLeds(`
             . . # . .
             . . . # .
@@ -25,8 +26,11 @@ function drive (mottatt: string) {
             . . . # .
             . . # . .
             `)
+        servos.P0.stop()
+        servos.P2.stop()
     } else if (mottatt == "Annet") {
-        servos.P1.run(-1 * max_fart)
+        servos.P2.run(-1 * max_fart)
+        servos.P0.run(max_fart)
         basic.showLeds(`
             . . # . .
             . # # # .
@@ -34,21 +38,28 @@ function drive (mottatt: string) {
             . . # . .
             . . # . .
             `)
-        servos.P0.run(max_fart)
-    } else if (mottatt == "stopp") {
-        servos.P1.stop()
+        basic.pause(2000)
+        servos.P2.stop()
         servos.P0.stop()
+    } else if (mottatt == "stopp") {
+        servos.P2.stop()
+        servos.P0.stop()
+        basic.showIcon(IconNames.Heart)
         basic.showString("S")
     }
+    mottatt = "Stopp"
 }
 radio.onReceivedString(function (receivedString) {
     mottatt = receivedString
+    basic.showIcon(IconNames.Heart)
     drive(mottatt)
 })
 let max_fart = 0
 let mottatt = ""
 let svingetid = 0
 radio.setGroup(1)
+radio.setTransmitPower(7)
 svingetid = 2000
 mottatt = ""
 max_fart = 20
+basic.showIcon(IconNames.Happy)
